2ee730cbac179b73ce766bec29da1562500377bf
Rafael Tejado <rtejado90@gmail.com>
2015-04-07 16:06:20 +0200

    Add ask_user_for_action method

--- basic_blog.rb
+++ basic_blog.rb
@@ -19,6 +19,7 @@ class Blog
 	def publish_front_page(page)
 		page.each{|post| give_format(post)}
 		add_numeration
+		ask_user_for_action
 	end
 
 	def add_numeration
@@ -40,6 +41,21 @@ class Blog
 		end
 	end
 
+	def ask_user_for_action
+		user_input = get_user_input
+		if user_input == "next" && @current_page_number != @pages.index(@pages.last)
+			advance_page
+		elsif user_input == "prev" && @current_page_number != @pages.index(@pages.first)
+			drop_page
+		end
+	end
+
+	def get_user_input
+		puts "What do you want to do? (NEXT, PREV, EXIT)"
+		user_input = gets.chomp.downcase
+	end
+
+
 	def advance_page
 		@current_page_number += 1
 		publish_front_page(@pages[@current_page_number])
@@ -49,6 +65,7 @@ class Blog
 		@current_page_number -= 1
 		publish_front_page(@pages[@current_page_number])
 	end
+
 
 
 	private
@@ -108,44 +125,27 @@ post2 = Post.new
 post2.title = "2. Goodbye World!"
 post2.text = "I don't love you any more"
 post3 = Post.new
-post3.title = "3"
+post3.title = "3. Not everything is lost"
 post3.text = "It's the third post"
+post3.sponsor = true
 post4 = Post.new
-post4.title = "4"
+post4.title = "4. Do you believe in sheeps?"
 post4.text = "I'm not creating more posts"
 
 
 blog = Blog.new(post1, post2, post3, post4)
 
-puts "\n\n\nI want a new page! \n\n\n\n\n"
-blog.advance_page
+# puts "\n\n\nWhat do you want to do? \n (NEXT, PREV, EXIT) \n\n\n\n\n".colorize(:red)
+# user_input = gets.chomp.downcase
+# if user_input == "next" && @current_page_number != @pages.index(@pages.last)
+# 		advance_page
+# 	elsif user_input == "prev" && @current_page_number != @pages.index(@pages.first)
+# 		drop_page
+# 	end
+
+# blog.advance_page
 
 
 puts "THE END"
 
 
-
-
-# def publish_front_page(*posts)
-# 		@posts[@current_page].each do |post|	
-# 			if post.sponsor
-# 				puts '******' + post.title + '******'
-# 			else
-# 				puts post.title
-# 			end
-# 			puts '********************'
-# 			puts post.text
-# 			puts '--------------------'
-# 		end
-# 		pages = @pages.map{|page| @posts.index(page) + 1}
-# 		pages.map! do |page| 
-# 			page = page.to_s
-# 			if page == (@current_page + 1).to_s
-# 				page = page.colorize(:blue)
-# 			else
-# 				page
-# 			end
-			
-# 		end
-# 		puts "Pages: " + pages.join(' ')
-# 	end
