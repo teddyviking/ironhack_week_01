993779fc9ad3ad27239542767372aa04e45f6bf9
Rafael Tejado <rtejado90@gmail.com>
2015-04-07 15:28:07 +0200

    Refactored for better understanding of the code

--- basic_blog.rb
+++ basic_blog.rb
@@ -3,60 +3,49 @@ require 'colorize'
 
 class Blog
 	attr_accessor :posts
-	attr_reader :current_page, :total_pages
+	attr_reader :current_page_number
 
-	def initialize
-		@posts = []
-		@current_page = 0
-	end
+	def initialize(*posts)
+		@posts = posts
+		@pages = []
+		@current_page_number = 0
 
-	def create_front_page(posts)
 		sort_posts(posts)
-		i = 0
-		page = []
-		posts.each do |post|
-			page << post
-			i += 1
-			if i % 3 == 0
-				@posts << page
-				page = []
-			elsif post == posts.last
-				@posts << page		
-			end
-		end
+		create_pages(posts)
+		publish_front_page(@pages[@current_page_number])
 	end
 
-	def publish_front_page(*posts)
-		create_front_page(posts)
-		@posts[@current_page].each do |post|	
-			if post.sponsor
-				puts '******' + post.title + '******'
+
+	def publish_front_page(page)
+		page.each{|post| give_format(post)}
+		add_numeration
+	end
+
+	def add_numeration
+		index_of_pages = []
+		@pages.each_index{|i| index_of_pages << (i + 1) }
+		# p index_of_pages
+		colorize_current_page(index_of_pages)
+		puts "Pages: " + index_of_pages.join(' ')
+	end
+
+	def colorize_current_page(index_of_pages)
+		index_of_pages.map! do |page_number|
+			page_number = page_number.to_s
+			if page_number  == (@current_page_number + 1).to_s
+				page_number = page_number.colorize(:blue)
 			else
-				puts post.title
+				page_number
 			end
-			puts '********************'
-			puts post.text
-			puts '--------------------'
 		end
-		pages = @posts.map{|page| @posts.index(page) + 1}
-		pages.map! do |page| 
-			page = page.to_s
-			if page == (@current_page + 1).to_s
-				page = page.colorize(:blue)
-			else
-				page
-			end
-			
-		end
-		puts "Pages: " + pages.join(' ')
 	end
 
 	def advance_page
-		@current_page += 1
+		@current_page_number += 1
 	end 
 
 	def drop_page
-		@current_page -= 1
+		@current_page_number -= 1
 	end
 
 
@@ -66,6 +55,31 @@ class Blog
 		posts.sort!{|post1, post2| post2.date <=> post1.date}
 	end
 
+	def create_pages(posts)	
+		i = 0
+		page = []
+		posts.each do |post|
+			page << post
+			i += 1
+			if i % 3 == 0
+				@pages << page
+				page = []
+			elsif post == posts.last
+				@pages << page		
+			end
+		end
+	end
+
+	def give_format(post)
+		if post.sponsor
+			puts '******' + post.title + '******'
+		else
+			puts post.title
+		end
+		puts '********************'
+		puts post.text
+		puts '--------------------'
+	end
 end
 
 
@@ -76,9 +90,13 @@ class Post
 	def initialize
 		@date = Time.now
 	end
+
+	def is_sponsored?
+		return true if @sponsor
+	end
 end
 
-blog = Blog.new
+
 
 post1 = Post.new
 post1.title = "1. Hello World!"
@@ -95,9 +113,35 @@ post4.title = "4"
 post4.text = "I'm not creating more posts"
 
 
-blog.publish_front_page(post1, post2, post3, post4)
+blog = Blog.new(post1, post2, post3, post4)
 blog.advance_page
-blog.publish_front_page(post1, post2, post3, post4)
 
 
 puts "THE END"
+
+
+
+
+# def publish_front_page(*posts)
+# 		@posts[@current_page].each do |post|	
+# 			if post.sponsor
+# 				puts '******' + post.title + '******'
+# 			else
+# 				puts post.title
+# 			end
+# 			puts '********************'
+# 			puts post.text
+# 			puts '--------------------'
+# 		end
+# 		pages = @pages.map{|page| @posts.index(page) + 1}
+# 		pages.map! do |page| 
+# 			page = page.to_s
+# 			if page == (@current_page + 1).to_s
+# 				page = page.colorize(:blue)
+# 			else
+# 				page
+# 			end
+			
+# 		end
+# 		puts "Pages: " + pages.join(' ')
+# 	end
