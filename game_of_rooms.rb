633bca183baecdfc13fa86e1b3ce2ad124147518
Rafael Tejado <rtejado90@gmail.com>
2015-04-08 18:57:20 +0200

    Add loop game and exit command to quit game in any time

--- game_of_rooms.rb
+++ game_of_rooms.rb
@@ -1,11 +1,13 @@
 require 'pry'
 class Game
 	attr_reader :rooms, :character, :reader
+	attr_accessor :win_condition
 
 
 	def initialize(*rooms)
 		@rooms ={}
-		@allowed_values = %w(N E S W sing)
+		@allowed_values = %w(N E S W sing quit)
+		@win_condition = false
 
 		rooms.each do |room|
 			@rooms[room.name] = room
@@ -14,9 +16,19 @@ class Game
 	end
 
 	def init_game
+		create_character
+		until @win_condition == true
+			display_current_location
+			apply_action(get_user_input)
+		end
+		get_end_of_game
+	end
+
+	def create_character
 		@character = Character.new(Room.new)
-		puts @character.location.description
-		apply_action(get_user_input)
+	end
+
+	def display_current_location
 		puts @character.location.description
 	end
 
@@ -24,10 +36,14 @@ class Game
 		puts "a>"
 		user_input = ""
 		until @allowed_values.include?(user_input)
-			puts "Sorry. You can't go this way" if user_input != ""
+			puts "Sorry. You can't do this" if user_input != ""
 			user_input = gets.chomp	
 		end
 		user_input
+	end
+
+	def get_end_of_game
+		puts "THE END"
 	end
 
 	def apply_action(user_input)
@@ -38,18 +54,23 @@ class Game
 	end
 	def select_action(user_input)
 		case user_input
-		when %w(N E S W).include?(user_input)	
-			"move"
+		when "N" || "E" || "S" || "W"	
+			return "move"
 		when "sing"
-			"sing"
+			return "sing"
+		when "quit"
+			puts "You have left the game"
+			exit
 		end
 	end
 	def make_action(action, user_input)
+		p action
 		if action == "sing"
-			@character.sing
+			@character.sing(@character.location)
 		elsif action == "move"
 			new_room = get_new_room(user_input, @character.location)
 			@character.move(new_room)
+			# set_new_allowed_values
 		end
 	end
 
@@ -71,6 +92,11 @@ class Game
 		end
 		return new_room.to_a[0][1]
 	end
+
+	# def set_new_allowed_values
+	# 	@allowed_values = []
+		
+	# end
 end
 
 class Character
@@ -87,22 +113,24 @@ class Character
 		@location = new_room
 	end
 
-	def sing
-		puts "LALALALA"
+	def sing(room)
+		puts room.responses_to_actions[:sing]
 	end
 
 end
 
 class Room
-	attr_reader :description, :name, :location, :available_moves
+	attr_reader :description, :name, :location, :available_moves, :responses_to_actions
 
-	def initialize(name="Kitchen", x=2, y=1, description="Estás en ", available_moves=["N","E"] )
+	def initialize(name="Kitchen", x=2, y=1, description="You are in ", available_moves=["N","E"] )
 		@name = name
 		@description = description + @name
 		@available_moves = available_moves
+		@responses_to_actions = {sing: "Some windows break because of your dreadful voice"}
 		@location ={x: x, y: y}
 	end
 	
+
 end
 
 living_room = Room.new("Living Room", 2, 2)
