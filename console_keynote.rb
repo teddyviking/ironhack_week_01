1ce334dd701a4a6100baeedd4abd1abb89d43e28
Rafael Tejado <rtejado90@gmail.com>
2015-04-09 13:12:25 +0200

    Create basic structure of Console Keynote

---
+++ console_keynote.rb
@@ -0,0 +1,37 @@
+class Keynote
+	attr_reader :slides
+
+	def begin_presentation
+		
+	end
+
+	def get_slides
+		
+	end
+
+	def select_slide
+		
+	end
+
+	def display_slide
+		
+	end
+
+	def change_slide
+		
+	end
+end
+
+class Slide
+	attr_reader :text
+
+	def get_text
+		
+	end
+
+	def format_text
+		
+	end
+
+end
+
