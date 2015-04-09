83d582a93b87f08339960777e689fb9715b4f525
Rafael Tejado <rtejado90@gmail.com>
2015-04-09 18:18:05 +0200

    Add cost method

--- shopping_cart.rb
+++ shopping_cart.rb
@@ -3,8 +3,9 @@ require 'pry'
 class ShoppingCart
 	attr_reader :items
 
-	def initialize
+	def initialize(prices)
 		@items = Hash.new { |hash, key| hash[key] = 1 }
+		@prices = prices
 
 		
 	end
@@ -14,22 +15,44 @@ class ShoppingCart
 	end
 
 	def cost
+		final_prices = set_final_prices
+		puts sum_prices(final_prices)
+	end
 
+	def set_final_prices
+		@items.merge(keep_present_prices) do |key, oldval, newval|
+			oldval * newval
+		end
+		
+	end
+
+	def keep_present_prices
+		@prices.objects.keep_if do |key, value|
+			@items.keys.map{|key| key.name}.include?(key)
+		end
+		
+	end
+
+	def sum_prices(items)
+		items.reduce(0) do |sum, (key, value)|
+			sum += value
+		end
 	end
 end
 
 class Item
 	attr_reader :name
 	def initialize(name)
-		@name = name
+		@name = name.to_sym
 	end
 end
 
 class PriceList
+	attr_reader :objects
 	def initialize
 		@objects = {
-			apples: 10,
-			oranges: 5,
+			apple: 10,
+			orange: 5,
 			banana: 20,
 			watermelon: 50,
 			rice: 1,
@@ -43,17 +66,14 @@ end
 
 
 
-
-
-
-
-cart = ShoppingCart.new
-price_list = PriceList.new
+cart = ShoppingCart.new(PriceList.new)
 apple = Item.new("apple")
 banana = Item.new("banana")
 
 cart.add apple
 cart.add banana
-binding.pry
+
 cart.cost 
+binding.pry
+puts "Frase chorra para pry: Elija la suya favorita"
 #=> 30
